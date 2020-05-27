7ebbea92c27ec1e0d152bdbfea934ba67ef9367b
Allan Moreno <allanm@nevada.unr.edu>
2020-05-27 10:09:40 -0700

    added background color,title, and image to landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react'; 
-
+import { Grid, Cell} from 'react-mdl';
 class Landing extends Component{
     render(){
         return(
-            <div><h1>Landing Page</h1></div>
+                  <div style ={{width: '100%', margin: 'auto'}}>
+                      <Grid className="landing-grid">
+                          <Cell col={12}>
+                          <img
+                            src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/man5-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                            />
+                             <div className= "banner-text">
+                                <h1>Software Developer</h1>
+                                
+                                <p>C++ | HTML/CSS | Python | React | Swift | C#</p>
+
+                                <hr/> 
+
+                             </div>
+
+                              
+                          </Cell>
+                      </Grid>
+                  </div>
         )
     }
 }
