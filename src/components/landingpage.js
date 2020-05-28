d698d1421f84931f42c6aa3aaff2b91b6669eaae
Allan Moreno <allanm@nevada.unr.edu>
2020-05-28 14:29:13 -0700

    Finished landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -24,8 +24,12 @@ class Landing extends Component{
                                 <div className= "social-links">
 
                                     {/*Linked in */}
-                                    <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <a href="https://www.linkedin.com/in/allan-moreno-5106a4187/" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                    </a>
+                                    {/* Github */}
+                                    <a href="https://github.com/Allan-O-Moreno?tab=repositories" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
                                     </a>
 
                                 
