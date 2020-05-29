4d037bb1051cf1269bdf2e27a106c3a2d5a00dc4
Allan Moreno <allanm@nevada.unr.edu>
2020-05-28 17:02:34 -0700

    added website code to projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,71 @@
-import React, {Component} from 'react'; 
 
-class Projects extends Component{
-    render(){
-        return(
-            <div><h1>Projects Page</h1></div>
-        )
-    }
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu} from 'react-mdl';
+
+
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+          {/* React Website */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Allans Website</CardTitle>
+            <CardText>
+              Allan's website created with React
+            </CardText>
+            <CardActions border>
+              <Button colored  href = "https://github.com/Allan-O-Moreno/portfolio_website" rel="noopener noreferrer" target="_blank">GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+
+        </div>
+
+
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>Swift</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>C++</h1></div>
+      )
+    } 
+    
+
+  }
+
+
+
+  render() {
+    return(
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>React</Tab>
+          <Tab>Swift</Tab>
+          <Tab>C++</Tab>
+        </Tabs>
+
+
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+
+
+      </div>
+    )
+  }
 }
 
-export default Projects 
+export default Projects;
