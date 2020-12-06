929fabcc8d5e40d81b825b65cff6cda18599d116
Allan Moreno <allanm@nevada.unr.edu>
2020-12-06 12:31:55 -0800

    Removed Resume page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
     <Layout>
     <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Home</Link>} scroll>
             <Navigation>
-                <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
@@ -20,7 +19,6 @@ function App() {
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Home</Link>}>
             <Navigation>
-                <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,7 @@ import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
-import Resume from './resume'; 
+// import Resume from './resume'; 
 
 
 import {Switch, Route} from 'react-router-dom';
@@ -15,7 +15,7 @@ const Main = () => (
         <Route path = "/aboutme" component = {AboutMe} /> 
         <Route path = "/contact" component = {Contact} />    
         <Route path = "/projects" component = {Projects} /> 
-        <Route path = "/resume" component = {Resume} />    
+        {/* <Route path = "/resume" component = {Resume} />     */}
     </Switch> 
 
 )
