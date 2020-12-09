79f08ea2a0b8d41e308826f8d18483c5c7ec98cb
Allan Moreno <allanm@nevada.unr.edu>
2020-12-08 20:15:53 -0800

    change navigation bar color and redsign about me, and landing page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -9,19 +9,23 @@ class About extends Component {
       <div className="aboutme-body">
         <Grid className="aboutme-grid">
           <Cell col={6}>
-            <h2>Allan Moreno</h2>
+            
             <img
               src={avatar2}
               alt="avatar"
               className ="avatar2-img"
               
                />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
-            I come from a little town from Winnemucca. I love to program and design software. During my freetime enjoy riding bicycles, 
-            playing basketball, and rollerskating. I have a Bachelor of Science in Computer Science and Engineering attained with a 3.3 GPA. 
-             
-             </p>
 
+              <div className = "about_block">
+               <h1 className = "about_description" >About Allan Moreno</h1>
+                 <p className = "about_description">
+                  I come from a little town from Winnemucca. I love to program and design software. During my freetime enjoy riding bicycles, 
+                  playing basketball, and rollerskating. I have a Bachelor of Science in Computer Science and Engineering attained with a 3.3 GPA. 
+             
+
+                 </p>
+              </div>
           </Cell>
           {/* <Cell col={6}>
             <h2>Hobbies</h2>
